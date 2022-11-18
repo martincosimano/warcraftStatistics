4780d2a7328c2be17264a145e0973299ba98343d
Martin Cosimano <martincosimano@hotmail.com>
2022-11-17 23:58:42 -0300

    created some variables to storage data from characters so the api can work globally

--- main.js
+++ main.js
@@ -2,52 +2,26 @@ document.querySelector('button').addEventListener('click', getFetch)
 let totalScore = 0
 
 function getFetch(){
-  const characterName = document.querySelector('input').value.toLowerCase()
-  console.log(characterName)
-  const url = ` https://classic.warcraftlogs.com:443/v1/rankings/character/${characterName}/benediction/us?api_key=94202797bf3c372e7d0f0dbc0b70996e`
+  const characterName = document.querySelector('#nameInput').value.toLowerCase()
+  const realmName = document.querySelector('#realmInput').value.toLowerCase()
+  let role = document.querySelector('#roleInput').value
+  role = role.replace(/^./, role[0].toUpperCase())
+  const raidSize = document.querySelector('#raidSizeInput').value
+
+  const url = ` https://classic.warcraftlogs.com:443/v1/rankings/character/${characterName}/${realmName}/us?api_key=94202797bf3c372e7d0f0dbc0b70996e`
 
   fetch(url)
 .then(res => res.json()) // parse response as JSON
 .then(data => {
   for(let i = 0;i <= data.length - 1;i++){
-    if(data[i].size === 25 && data[i].spec === "Retribution" && data[i].encounterName !=="Gothik the Harvester"){
+    if(data[i].size === Number(raidSize) && data[i].spec === role.toString() && data[i].encounterName !=="Gothik the Harvester"){
       console.log(data[i].encounterName,Math.ceil(data[i].percentile))
       totalScore += (Math.ceil(data[i].percentile) / 16)
     }
   }
-  console.log(Math.round(totalScore))
+  console.log(`${characterName.replace(/^./, characterName[0].toUpperCase())} score is ${Math.round(totalScore)}`)
 })
 .catch(err => {
     console.log(`error ${err}`)
 })
 }
-
-// function getFetch(){
-//   const characterName = document.querySelector('input').value.toLowerCase()
-//   console.log(characterName)
-//   const url = ` https://classic.warcraftlogs.com:443/v1/rankings/character/${characterName}/benediction/us?metric=bossdps&api_key=94202797bf3c372e7d0f0dbc0b70996e`
-
-//   fetch(url)
-// .then(res => res.json()) // parse response as JSON
-// .then(data => {
-//   console.log(data)
-// })
-// .catch(err => {
-//     console.log(`error ${err}`)
-// })
-// }
-
-// function getFetch(){
-//   const characterName = document.querySelector('input').value.toLowerCase()
-//   console.log(characterName)
-//   const url = ` https://classic.warcraftlogs.com:443/v1/rankings/character/${characterName}/benediction/us?api_key=94202797bf3c372e7d0f0dbc0b70996e`
-
-//   fetch(url)
-// .then(res => res.json()) // parse response as JSON
-// .then(data => {
-//   console.log(data)
-// })
-// .catch(err => {
-//     console.log(`error ${err}`)
-// })
-// }
