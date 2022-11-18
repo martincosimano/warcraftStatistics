eee18928d6105871102e92e8cb9645e8b01d026f
Martin Cosimano <martincosimano@hotmail.com>
2022-11-17 21:24:47 -0300

    working on the data getter

--- main.js
+++ main.js
@@ -9,13 +9,45 @@ function getFetch(){
   fetch(url)
 .then(res => res.json()) // parse response as JSON
 .then(data => {
-  console.log(data)
-  for(let i = 0;i < data.length - 1;i++){
-    console.log(data[i].encounterName,data[i].percentile)
-    totalScore += Number(data[i].percentile) / (data.length -1)
+  for(let i = 0;i <= data.length - 1;i++){
+    if(data[i].size === 25 && data[i].spec === "Retribution" && data[i].encounterName !=="Gothik the Harvester"){
+      console.log(data[i].encounterName,Math.ceil(data[i].percentile))
+      totalScore += (Math.ceil(data[i].percentile) / 16)
+    }
   }
+  console.log(Math.round(totalScore))
 })
 .catch(err => {
     console.log(`error ${err}`)
 })
 }
+
+// function getFetch(){
+//   const characterName = document.querySelector('input').value.toLowerCase()
+//   console.log(characterName)
+//   const url = ` https://classic.warcraftlogs.com:443/v1/rankings/character/${characterName}/benediction/us?metric=bossdps&api_key=94202797bf3c372e7d0f0dbc0b70996e`
+
+//   fetch(url)
+// .then(res => res.json()) // parse response as JSON
+// .then(data => {
+//   console.log(data)
+// })
+// .catch(err => {
+//     console.log(`error ${err}`)
+// })
+// }
+
+// function getFetch(){
+//   const characterName = document.querySelector('input').value.toLowerCase()
+//   console.log(characterName)
+//   const url = ` https://classic.warcraftlogs.com:443/v1/rankings/character/${characterName}/benediction/us?api_key=94202797bf3c372e7d0f0dbc0b70996e`
+
+//   fetch(url)
+// .then(res => res.json()) // parse response as JSON
+// .then(data => {
+//   console.log(data)
+// })
+// .catch(err => {
+//     console.log(`error ${err}`)
+// })
+// }
